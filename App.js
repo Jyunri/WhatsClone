be845fbee49aa6a06f49d37c66df5be31a8ce657
Jyunri <nekojimy@gmail.com>
2018-08-05 20:29:16 -0300

    add reducers

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import Routes from './src/components/Routes';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux';
+import reducers from './src/reducers'
 
 export default class App extends Component {
   render() {
